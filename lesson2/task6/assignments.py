2b0a931199b10b29605f114a35ee20a28d2072d3
sergio9977 <sergio9961937@gmail.com>
2018-02-02 16:08:04 -0400

    CSLM Subiendo leccion 2

---
+++ lesson2/task6/assignments.py
@@ -0,0 +1,13 @@
+"""
+La asignación aumentada es una declaración única que combina
+una operación binaria y una declaración de asignación como "+=", "-="
+"""
+number = 9.0
+print("number = " + str(number))
+
+number -= 2
+print("number = " + str(number))
+
+number += 5
+
+print("number = " + str(number))
---
+++ lesson2/task8/comparison_operators.py
@@ -0,0 +1,19 @@
+"""
+Python tiene muchos tipos de operadores de comparación, incluidos
+>=,
+<=,
+>,
+<, etc.
+Todas las operaciones de comparación en Python tienen la misma prioridad.
+Las comparaciones arrojan valores booleanos: True o False.
+Las comparaciones pueden encadenarse arbitrariamente.
+
+"""
+one = 1
+two = 2
+three = 3
+
+print(one < two < three)
+
+is_greater = three > two
+print(is_greater)
