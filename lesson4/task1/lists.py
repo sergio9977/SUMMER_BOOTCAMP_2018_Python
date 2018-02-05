08ca1fad156a00059454cb43528bd422bcd265de
sergio9977 <sergio9961937@gmail.com>
2018-02-05 16:10:43 -0400

    CSLM Tarea completa de la leccion 4

---
+++ lesson4/task1/lists.py
@@ -0,0 +1,20 @@
+"""
+Una lista es una estructura de datos que se puede usar para almacenar
+una colección de diferentes partes de información
+bajo un solo nombre de variable.
+Las listas pueden contener elementos de diferentes tipos,
+pero generalmente todos los elementos de la lista son del mismo tipo.
+Al igual que las cadenas, las listas se pueden indexar y dividir.
+"""
+squares = [1, 4, 9, 16, 25]
+print(squares)
+
+print(squares[1:4])
+
+# tarea obtener la primera mitad
+
+print(len(squares)/2)
+
+mitad = squares[:int(len(squares)/2)]
+print(mitad)
+
